3868c35fe7a5779e5213cdbcfd1d8911bcbe13b2
Marlon Isaac Castro Sanchez <marlon.castro25u@std.uni.edu.ni>
2022-11-13 12:20:51 -0600

    deveritas deveritas ke me mato aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- El-Proyecto/CONTROLADOR/Controlador.cs
+++ El-Proyecto/CONTROLADOR/Controlador.cs
@@ -1,4 +1,5 @@
 ﻿using MODELO;
+using System.Windows.Forms;
 namespace CONTROLADOR
 {
     public class Contrl
@@ -9,6 +10,29 @@ namespace CONTROLADOR
         public void verDatos(DataGridView dgvDatos)
         {
             dgvDatos.Rows.Add(donante.Apellido, donante.Nombre, donante.Sexo, donante.F_Nac, donante.Peso,sangre.Grupo, sangre.Rh);
+        }
+        public void SaveContact(TextBox nombre, TextBox apellido, TextBox cedula,DateTimePicker fecha ,RadioButton femenino, RadioButton masculino, TextBox peso, ComboBox RH, ComboBox grupoSanguineo, TextBox centroDonacion, DataGridView dgvDatos)
+        {
+
+            string name = nombre.Text;
+            string subname = apellido.Text;
+            string cedul4 = cedula.Text;
+            string pes0 = peso.Text;
+            string fech4 = fecha.Text;
+            string genero = null;
+            string rh = RH.Text;
+            string centro = centroDonacion.Text;
+            string grupoSangre = grupoSanguineo.Text;
+            if (femenino.Checked)
+            {
+                genero = "femenino";
+            }
+            else if (masculino.Checked)
+            {
+                genero = "masculino";
+            }
+            verDatos(dgvDatos);
+            MessageBox.Show("Contacto Agregado");
         }
     }
 }
